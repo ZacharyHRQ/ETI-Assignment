4bd7ea6767eba8d655f1b8ca69c1d87690d4424a
zachary <zac.hong01@gmail.com>
2021-12-01 17:41:33 +0800

    added sql folder

---
+++ sql/passenger.sql
@@ -0,0 +1,11 @@
+Use Ridely;
+
+CREATE TABLE Passenger (PassengerId int  PRIMARY KEY auto_increment, FirstName VARCHAR(60), LastName VARCHAR(60), MoblieNo VARCHAR(8), EmailAddress VARCHAR(120)); 
+
+INSERT INTO Passenger (FirstName, LastName, MoblieNo, EmailAddress) VALUES ("Jake", "Lee", "87394723","jakelee@gmail.com");
+INSERT INTO Passenger (FirstName, LastName, MoblieNo, EmailAddress) VALUES ("Gay", "Lee", "87394723","jakelee@gmail.com");
+INSERT INTO Passenger (FirstName, LastName, MoblieNo, EmailAddress) VALUES ("Jake1", "Lee", "87394723","jakelee@gmail.com");
+INSERT INTO Passenger (FirstName, LastName, MoblieNo, EmailAddress) VALUES ("Joe", "Lee", "87394723","jakelee@gmail.com");
+INSERT INTO Passenger (FirstName, LastName, MoblieNo, EmailAddress) VALUES ("John", "Lee", "87394723","jakelee@gmail.com");
+
+Select * From Passenger;
